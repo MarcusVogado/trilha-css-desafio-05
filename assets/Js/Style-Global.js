e7bd8ff37d3e902403c909198b37e11902b8d97b
MarcusVogado <marcus_620@hotmail.com>
2023-07-19 18:49:06 -0300

    update project nav-header and header added

--- assets/Js/Style-Global.js
+++ assets/Js/Style-Global.js
@@ -1,11 +1,12 @@
-var nav = document.getElementById('nav_header'); 
-var numPx = '250'; 
+var nav = document.getElementById('nav-header'); 
+var numPx = '50'; 
 
 window.addEventListener('scroll', function() {
     if (window.scrollY > numPx) {
     	nav.classList.add('nav__header-transparent');
     } else {
       nav.classList.remove('nav__header-transparent');
+      
     }
 });
 
